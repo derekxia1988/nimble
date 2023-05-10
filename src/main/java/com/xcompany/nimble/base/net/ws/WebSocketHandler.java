54ae5fb4c19c66bc55d2d51b3b765ed0836c40db
xiadong <xiadong@topjoy.com>
2023-05-10 14:34:42 +0800

    Desc: add example for biz executor and numerics Type: feat Func: base Author: xiadong

--- src/main/java/com/xcompany/nimble/base/net/ws/WebSocketHandler.java
+++ src/main/java/com/xcompany/nimble/base/net/ws/WebSocketHandler.java
@@ -1,6 +1,9 @@
 package com.xcompany.nimble.base.net.ws;
 
 import com.alibaba.fastjson.JSONObject;
+import com.xcompany.nimble.base.executor.OrderedExecutor;
+import com.xcompany.nimble.biz.numeric.NumericTable;
+import com.xcompany.nimble.biz.numeric.Numerics;
 import lombok.extern.log4j.Log4j2;
 import org.springframework.context.ApplicationEventPublisher;
 import org.springframework.stereotype.Component;
@@ -19,9 +22,11 @@ import java.time.LocalDateTime;
 @Log4j2
 public class WebSocketHandler extends AbstractWebSocketHandler {
     private final ApplicationEventPublisher publisher;
+    private final OrderedExecutor<String> bizExecutor;
 
-    public WebSocketHandler(ApplicationEventPublisher publisher) {
+    public WebSocketHandler(ApplicationEventPublisher publisher, OrderedExecutor<String> bizExecutor) {
         this.publisher = publisher;
+        this.bizExecutor = bizExecutor;
     }
 
     @Override
@@ -37,6 +42,14 @@ public class WebSocketHandler extends AbstractWebSocketHandler {
         // 获得客户端传来的消息
         String payload = message.getPayload();
         log.info("server 接收到消息 " + payload);
+
+        // 这是业务线程池的例子
+        bizExecutor.execute(session.getId(), () -> {
+            log.info("这是在业务线程里执行的代码");
+            log.info("server 接收到消息 " + payload);
+            // 这是去策划数据的例子
+            Numerics.get(NumericTable.class, 1);
+        });
         publisher.publishEvent(new WSReqEvent(null, JSONObject.parseObject(payload)));
         session.sendMessage(new TextMessage("server 发送给的消息 " + payload + "，发送时间:" + LocalDateTime.now().toString()));
     }
