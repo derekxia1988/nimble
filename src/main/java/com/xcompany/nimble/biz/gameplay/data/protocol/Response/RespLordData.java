3a581d4cb0a8b239a9fc045f286fe9584658e30d
fucan <fucan@elysium.com>
2023-05-11 20:41:44 +0800

    add lordlevel

--- src/main/java/com/xcompany/nimble/biz/gameplay/data/protocol/Response/RespLordData.java
+++ src/main/java/com/xcompany/nimble/biz/gameplay/data/protocol/Response/RespLordData.java
@@ -1,8 +1,10 @@
 package com.xcompany.nimble.biz.gameplay.data.protocol.Response;
 
 import lombok.Builder;
+import lombok.Data;
 
 @Builder
+@Data
 public class RespLordData implements RespData{
     private int lordLevel = 0;
 }
